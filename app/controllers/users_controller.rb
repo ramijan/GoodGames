a9e0f77313dd4d26b2e3a5c74138653b90413872
ramiburpee <rburpee@gmail.com>
2015-01-14 21:55:13 -0800

    Add sessions controller and login form page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,8 @@ class UsersController < ApplicationController
     end
   end
 
-
-
-
-
   private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
-
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'application#hello'
 
+  get '/login' => 'sessions#new', as: :login
+  post '/login' => 'sessions#create'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
