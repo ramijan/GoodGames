f7ae05212d86c7dfd57b42513c667a6f62f20549
ramiburpee <rburpee@gmail.com>
2015-01-14 21:39:33 -0800

    Update user model, user routes and make the Create account form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
+  
   field :first_name, type: String
   field :last_name, type: String
-  field :username, type: String
+  field :email, type: String
   field :password_digest, type: String
+
+  has_secure_password
 end
