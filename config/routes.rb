e6c89b5e6dcc2f77bf27b6d661ac78d8cee47758
ramiburpee <rburpee@gmail.com>
2015-01-14 22:07:54 -0800

    Implement sessions#destroy + route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new', as: :login
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
