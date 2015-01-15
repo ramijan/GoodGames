2d7bec069d7b8efb0eaaa5a3d76bdf013ee2acf2
ramiburpee <rburpee@gmail.com>
2015-01-15 00:28:13 -0800

    Small improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to login_path
     else
       render :new
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User
   field :password_digest, type: String
 
   has_secure_password
+
+  validates :first_name, :last_name, :email, presence: true
 end
