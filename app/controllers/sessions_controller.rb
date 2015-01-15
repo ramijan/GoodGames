738d5300bcf7b9109ee7474ebc1de5f287ebad50
ramiburpee <rburpee@gmail.com>
2015-01-14 22:03:45 -0800

    Implement sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id.to_s
+      redirect_to login_path
+    else
+      render :new
+    end
+
   end
 
   def destroy
