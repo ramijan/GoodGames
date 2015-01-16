795c144cdc6d192fc49e6ed25e78d135de9164cb
ramiburpee <rburpee@gmail.com>
2015-01-16 00:52:18 -0800

    Add search bar that searches and displays giant bomb results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,18 @@
-class GamesController < ApplicationController
-  before_action :authorize
+require 'net/http'
+require 'json'
 
+class GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    if params[:query] && params[:query] != ''
+      url = URI.parse("http://www.giantbomb.com/api/search/?api_key=b038c70b0015dfe2aa7b79b78386907fb0f2d850&format=json&query=#{params[:query]}&resources=game&field_list=name,image,deck,original_release_date,platforms,id")
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port) {|http| http.request(req)}
+      @games = JSON.parse(res.body)['results']
+      # @games = Game.where(:name => /.*#{params[:query]}.*/)
+    else
+      @games = {}
+    end
   end
 
   def show
