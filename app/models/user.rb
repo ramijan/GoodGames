b06857a356c0c216011418dcc0d58e41e4ea7276
ramiburpee <rburpee@gmail.com>
2015-01-19 15:48:24 -0800

    Add logic to games list for 'list' column, so buttons are not shown for games already added to lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,18 @@ class User
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+
+  def get_list_location(game)
+    if self.games_played.include?(game.id.to_s)
+      return :games_played
+    elsif self.games_playing.include?(game.id.to_s)
+      return :games_playing
+    elsif self.games_want.include?(game.id.to_s)
+      return :games_want
+    else
+      return nil
+    end
+  end
+
+
 end
