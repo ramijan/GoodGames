637718decfc7bf21cb36d1fc735a875e0eaf1a16
ramiburpee <rburpee@gmail.com>
2015-01-19 17:19:14 -0800

    Update search results page links to be smarter (recognizing that user has game in collection).  Update game detail page. Update game list page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,13 @@
 class SearchController < ApplicationController
 
   def results
+    @all_games = Game.all
+    @my_games = current_user.games
+
     if params[:query] && params[:query] != ''
       @results = get_giant_bomb_data(params[:query], 'name,image,api_detail_url,original_release_date')
+    else
+      @results = []
     end
   end
 
